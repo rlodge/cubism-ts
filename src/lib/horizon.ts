ce458b8d6ed8abc53d3b82891ef463e98a699b55
Ross Lodge <rlodge@concentricsky.com>
2021-02-23 16:41:29 -0800

    Fix warning

--- src/lib/horizon.ts
+++ src/lib/horizon.ts
@@ -140,7 +140,7 @@ export class Horizon {
 
           const canvasContext = maybeCanvasContext;
 
-          function change(start1In: Date | number) {
+          const change = function(start1In: Date | number) {
             const start1 = +start1In;
             canvasContext.save();
 
